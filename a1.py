4e30ceca334e024171d8cd1a9567fde0d1dc94bf
isha123321 <isha101112@hotmail.com>
2024-12-15 10:07:12 +0500

    first commit

---
+++ a1.py
@@ -0,0 +1,6 @@
+a = int(input("Enter the first number (a): "))
+b = int(input("Enter the second number (b): "))
+c = int(input("Enter the third number (c): "))
+
+a, b, c = swap_three_numbers(a, b, c)
+print("After swapping: a = {a}, b = {b}, c = {c}")
